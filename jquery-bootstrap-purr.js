e001c40941870a8aa003ca58a8dbd35b2ef13a41
softwarespot <softwarespotapps@gmail.com>
2015-09-05 22:48:26 +0300

    v1.1.1 released

--- jquery-bootstrap-purr.js
+++ jquery-bootstrap-purr.js
@@ -3,7 +3,7 @@
  * https://github.com/softwarespot/jquery-bootstrap-purr
  * Author: softwarespot
  * Licensed under the MIT license
- * Version: 1.0.0
+ * Version: 1.1.1
  */
 ; // jshint ignore:line
 (function ($, undefined) {
